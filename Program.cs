c697251cad73353378f9ba55d9113a319ceb2513
Ambili das v s <ambili.kan@gmail.com>
2022-05-30 12:15:11 +0530

    Binary search Tree Implimentation Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySearchTreeCreation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome to Binary Search Tree Program");
+            Console.ReadKey();
+        }
+    }
+}
